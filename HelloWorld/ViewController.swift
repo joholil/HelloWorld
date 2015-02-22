274ce21d534d5cddd08f7bc91514e6d4868a65a0
Johan Hogberg <joholil@hotmail.com>
2015-02-22 11:49:28 +0100

    Här har jag lagt till en funktion för att lägga på ett filter på en bild

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var PhotoImageView: UIImageView!
+    
+    // Create a place to render the image
+    let context = CIContext(options: nil)
+    
+    
+    @IBAction func appFilter(sender: AnyObject) {
+    
+
+        let inputImage = CIImage(image: PhotoImageView.image)
+        let radomColor = [kCIInputAngleKey: (Double(arc4random_uniform(314)))]
+        
+        let filterImage = inputImage.imageByApplyingFilter( "CIHueAdjust", withInputParameters: radomColor)
+        let renderedImage = context.createCGImage(filterImage, fromRect: filterImage.extent())
+        
+        PhotoImageView.image = UIImage(CGImage:renderedImage)
+        
+    }
+    
+    @IBAction func test(sender: AnyObject) {
+    
+    
+    
+    
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,4 +48,3 @@ class ViewController: UIViewController {
 
 
 }
-
